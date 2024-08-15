5af1d10e739a1b59b3930245aec57a365fca3a55
saadkhatri1 <khatrisaad233@gmail.com>
2024-08-15 22:50:20 +0530

    public variables

--- app/owbh/route.js
+++ app/owbh/route.js
@@ -1,11 +1,11 @@
 import { Resend } from "resend"
 export async function GET(req, res) {
   try {
-    const resend = new Resend(process.env.resend_apiKey)
+    const resend = new Resend(process.env.NEXT_PUBLIC_resend_apiKey)
     await resend.emails.send({
-      from: `Saad Khatri (Ratan) <${process.env.email}>`,
+      from: `Saad Khatri (Ratan) <${process.env.NEXT_PUBLIC_email}>`,
       subject: `New Order From`,
-      to: process.env.email,
+      to: process.env.NEXT_PUBLIC_email,
       html: `order webhook`,
     })
     return new Response("Success", {
