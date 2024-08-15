a66781f08ec2ee6d1e293515994e7905f0bc2422
saadkhatri1 <khatrisaad233@gmail.com>
2024-08-15 22:35:36 +0530

    await in resend

--- app/owbh/route.js
+++ app/owbh/route.js
@@ -2,7 +2,7 @@ import { Resend } from "resend"
 export async function GET(req, res) {
   try {
     const resend = new Resend(process.env.resend_apiKey)
-    resend.emails.send({
+    await resend.emails.send({
       from: process.env.email,
       subject: `New Order From ${req.body.username}`,
       to: process.env.email,
