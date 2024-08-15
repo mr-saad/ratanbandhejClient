b28f51ddcdde3ad0648f5d07e1b4d17d2a8a9eb9
saadkhatri1 <khatrisaad233@gmail.com>
2024-08-16 00:29:05 +0530

    testing webh

--- app/owbh/route.js
+++ app/owbh/route.js
@@ -2,15 +2,17 @@ import { Resend } from "resend"
 export async function GET(req, res) {
   try {
     const resend = new Resend(process.env.resend_apiKey)
-    await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: `Saad Khatri (Ratan) <onboarding@resend.dev>`,
       subject: `New Order From`,
       to: process.env.email,
       html: `new order from webhook`,
     })
-    return new Response("Success", {
-      status: 200,
-    })
+    if (error) {
+      return new Response(error.message)
+    } else {
+      return new Response(data.id)
+    }
   } catch (error) {
     return new Response(JSON.stringify(error), {
       status: 500,
