d13275f167e91b030ecfa785183474d67655774d
saadkhatri1 <khatrisaad233@gmail.com>
2024-11-01 17:57:41 +0530

    dynamic about,contact,terms,privacy

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,3 @@
-export const dynamic = "force-static"
-
 export const metadata = {
   title: "Contact",
   keywords: ["contact", "contactus"],
--- app/verify-account/route.js
+++ app/verify-account/route.js
@@ -23,7 +23,7 @@ export async function GET(req) {
           },
         ])
 
-        if (res?.transctionId) {
+        if (res?.transactionId) {
           redirect("/sign-in")
         } else {
           return new Response(JSON.stringify(res))
