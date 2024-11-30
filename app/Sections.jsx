4f64e0978883806c9b627485e72f1bcbd83fa482
saadkhatri1 <khatrisaad233@gmail.com>
2024-11-30 19:06:06 +0530

    back to ppr

--- app/Sections.jsx
+++ app/Sections.jsx
@@ -1,7 +1,14 @@
 import Link from "next/link"
 import Product from "@/components/Product"
+import getProducts from "@/lib/getProducts"
 
-export default async function Sections({ dupatta, saree, dress, topMaterial }) {
+export default async function Sections() {
+  const [dupatta, saree, dress, topMaterial] = await Promise.all([
+    getProducts({ type: "Dupatta", count: 2 }),
+    getProducts({ type: "Saree", count: 2 }),
+    getProducts({ type: "Dress", count: 2 }),
+    getProducts({ type: "Top Material", count: 2 }),
+  ])
   return (
     <>
       <Section title={dupatta[0].type} data={dupatta} />
--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,20 @@
 import HeaderImage from "./HeaderImage"
 import Sections from "./Sections"
 import { Quagera } from "@/components/logoFont"
-import CarouselClient from "./CarouselClient"
-import getProducts from "@/lib/getProducts"
+import Carousel from "./Carousel"
+import { Suspense } from "react"
+import CarouselLoading from "@/components/loadings/CarouselLoading"
+import ProductsLoading from "@/components/loadings/ProductsLoading"
 
-export const revalidate = 3600
+export const experimental_ppr = true
 
-export default async function Home() {
-  const carousel = await getProducts({ count: 4 })
-  const [dupatta, saree, dress, topMaterial] = await Promise.all([
-    getProducts({ type: "Dupatta", count: 2 }),
-    getProducts({ type: "Saree", count: 2 }),
-    getProducts({ type: "Dress", count: 2 }),
-    getProducts({ type: "Top Material", count: 2 }),
-  ])
-
+export default function Home() {
   return (
     <>
       <header className="relative flex min-h-[95vh] w-full items-center bg-[#111] before:absolute before:z-[2] before:h-full before:w-full before:bg-gradient-to-l before:from-transparent before:to-[#111]/90">
-        <HeaderImage />
+        <Suspense fallback="">
+          <HeaderImage />
+        </Suspense>
         <div className="z-10 px-5 text-white md:px-20">
           <h1
             className={
@@ -34,14 +30,13 @@ export default async function Home() {
       <div className="Container mx-auto">
         <div className="home mb-10 overflow-hidden">
           <h1 className="heading arrivals my-5 !text-4xl">Fresh Crafts</h1>
-          <CarouselClient data={carousel} />
+          <Suspense fallback={<CarouselLoading />}>
+            <Carousel />
+          </Suspense>
         </div>
-        <Sections
-          dupatta={dupatta}
-          saree={saree}
-          dress={dress}
-          topMaterial={topMaterial}
-        />
+        <Suspense fallback={<ProductsLoading />}>
+          <Sections />
+        </Suspense>
       </div>
     </>
   )
