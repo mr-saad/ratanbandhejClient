9c8951496f3df4cb30b5276b1d15c9cd09ceeac6
saadkhatri1 <khatrisaad233@gmail.com>
2026-08-26 23:17:19 +0530

    disable HeaderImage for testing

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <header className="relative flex h-[95vh] items-center before:absolute before:z-2 before:h-full before:w-full before:bg-linear-to-r before:from-stone-950/90 before:to-transparent">
-        <HeaderImage />
+        {/*<HeaderImage />*/}
         <div className="z-10 mx-auto grow px-5 text-white lg:max-w-7xl">
           <h1 className="font-quagera text-7xl font-bold [text-shadow:0_0_10px_rgba(0,0,0,0.5)]">
             Ratan Bandhej
